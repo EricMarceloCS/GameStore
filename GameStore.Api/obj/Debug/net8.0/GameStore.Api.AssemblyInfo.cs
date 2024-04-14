f732dd40ef99a15ea9a2f01e247e20313cbe4bb6
eric <ericmarcelo455@gmail.com>
2024-04-13 17:26:10 -0700

    Dependency Injection
    * Created GameDetailsDTO
    * change GameDto -> GameSummaryDto
    * Create Mapping from Dto to Entity
    * Create Mapping from Entity to Dto
    * Make CRUD Operations

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b013f39a424c42be0499aa6903f0d7863bd43f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4c5dc14e733ea153e5bd1f352270d1d4d34b3c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
